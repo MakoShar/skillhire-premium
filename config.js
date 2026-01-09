1aaaaeb4ebc228ca627234ef12250ef39454c827
Yash Jain <06jainyash@gmail.com>
2026-01-09 18:30:29 +0530

    updated

---
+++ config.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAHzcOBT1k0z2zzoxdDehyV8OaChZwnWy4",
+    authDomain: "skill-hire-test.firebaseapp.com",
+    projectId: "skill-hire-test",
+    storageBucket: "skill-hire-test.firebasestorage.app",
+    messagingSenderId: "217750622906",
+    appId: "1:217750622906:web:5426dbebfd8ea47f0ee883",
+    measurementId: "G-5YGX4KTSL9"
+};
+
+var geminiConfig = {
+    apiKey: "AIzaSyCAH3Sj-XxcKAhGOFmKrfvxlpL5yS9rEmE"
+};
